edf2f0de4ca5b5c7a4e87c83df469e2472050095
Isha Rathod <ishadrathod28121308@gmail.com>
2025-11-18 00:04:21 +0530

    Added changes in base URL and android URL in flutter for production for issue

--- frontend/lib/config/api_config.dart
+++ frontend/lib/config/api_config.dart
@@ -1,9 +1,9 @@
 class ApiConfig {
   // Base URL for production API on EC2
-  static const String baseUrl = 'http://0.0.0.0:3000/api/v1';  // Replace with your EC2 public IP
+  static const String baseUrl = 'http://16.176.192.120:3000/api/v1';  // EC2 Public IP
   
   // Optional: Android emulator (still points to EC2)
-  static const String androidBaseUrl = 'http://0.0.0.0:3000/api/v1';
+  static const String androidBaseUrl = 'http://16.176.192.120:3000/api/v1';
   
   // Endpoints
   static const String transactionsEndpoint = '$baseUrl/transactions';
